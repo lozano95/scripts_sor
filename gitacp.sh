1eb63aad138093af26bb42409d8580151b1dc82c
victor <victor@victor.fpmislata.local>
2013-12-11 16:38:14 +0100

    añadido fichero pepe.txt

--- gitacp.sh
+++ gitacp.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 git add $1
 git commit -a -m "añadido fichero $1"
-git push origin master 
+if [ "$2" == "bitbucket" ]; then
+	 origen=origin2
+else
+	origen=origin
+fi 
+	    
+git push -u $origen master 
