ede9f072594e223075525fb44ba8010865c2a829
victor <victor1@ubuntu.fpmislata.local>
2014-01-28 20:09:10 +0100

    te dice pirulas del pc

---
+++ showcachecpu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cache=$(cat /proc/cpuinfo | grep "cache size" | cut -d ":" -f2)
+procesador=$(cat /proc/cpuinfo | grep "model name" | cut -d ":" -f2)
+fpu=$(cat /proc/cpuinfo | grep -P "^fpu\t" | cut -d ":" -f2)
+echo "el procesador $procesador tiene una cache de $cache"  
+if [ "$fpu" == " yes" ]; then 
+echo si que tiene fpu
+fi
