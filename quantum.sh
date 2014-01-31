b630e602ff02b7e0f978c15f149cb96790343025
root <root@ubuntu.fpmislata.local>
2014-01-31 15:59:10 +0100

    quantum echo

--- quantum.sh
+++ quantum.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 pid=$1
-nice=$(ps -eo pid,nice | grep -P "^ *$pid " | cut -c5
+nice=$(ps -eo pid,nice | grep -P "^ *$pid " | cut -c7-9)
+echo "El valor nice es $nice"
+
+if [ "$nice" -ge "0" ]; then
+	echo "El numero es 0 o mayor"
+	quantum=$[(20-$nice)*5]
+else
+	echo "El numero es menos que 0"
+	quantum=$[(20-$nice)*20]
+fi
+
+echo "El quantum es $quantum"
